a4c8ab57e10e3206069cda73068f1a162ea9575d
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-22 03:34:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function showRepositories(event, data) {
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/minidelta66/repos');
+  req.open('GET', 'https://api.github.com/users/enginBozkurt/repos');
   req.send();
 }
+
